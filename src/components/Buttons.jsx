2ae2617aa9f6fea88e6cde8012e639c2dad6efa2
erikn775 <erikn775@gmail.com>
2021-03-15 14:46:59 -0600

    added rsuite

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import { Nav, Icon } from 'rsuite';
+import 'rsuite/dist/styles/rsuite-default.css';
 
 class Buttons extends React.Component{
 
     render(){
         return(
+            // <Nav>
+            //     <Nav.Item icon={<Icon icon="home" />}>
+            //         <Nav.Link href="/car/new">Car Sustainability Helper</Nav.Link>
+            //     </Nav.Item>
+            //     <Nav.Item>
+            //         <Nav.Link href="/home/new">Home Sustainability Helper</Nav.Link>
+            //     </Nav.Item>
+            // </Nav>
             <>
-                <Link to="/car/new">Car Sustainability Helper</Link><br/>
-                <Link to="/home/new">Home Sustainability Helper</Link>
+            <NavLink to="/car/new">Car Sustainability Helper</NavLink><br/>
+            <NavLink to="/home/new">Home Sustainability Helper</NavLink>
             </>
         )
     }
