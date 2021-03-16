feb7540892c287b56221b5b9f71bcd1c3b74f043
erikn775 <erikn775@gmail.com>
2021-03-16 16:11:21 -0600

    car from styling changes

--- src/components/CarForm.jsx
+++ src/components/CarForm.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {Redirect} from 'react-router-dom'
-import { Form, FormGroup, FormControl, ControlLabel, HelpBlock, Button, ButtonToolbar, Dropdown } from 'rsuite';
+//import { Form, FormGroup, FormControl, ControlLabel, HelpBlock, Button, ButtonToolbar, Dropdown } from 'rsuite';
+import {Form, Button} from 'react-bootstrap';
 import 'rsuite/dist/styles/rsuite-default.css';
+import '../carform.css'
 
 
 class CarForm extends React.Component{
@@ -46,82 +48,86 @@ class CarForm extends React.Component{
                 }}
             />
         }
+        // return(
+        //     <>
+        //         <h2 className="car-form-title">Car Information</h2>
+        //         <form onSubmit={this.formSubmit} className="car-form">
+        //             <input onChange={this.onFormChange} className="car-name" name="name" type="text" placeholder="Enter Name..."/><br/>
+        //             <input onChange={this.onFormChange} className="car-email" name="email" type="text" placeholder="Enter Email..."/><br/>
+        //             <select onChange={this.onFormChange} className="car-ownership" name="ownACar">
+        //                 <option value="">Do you own a car?</option>
+        //                 <option value="true">Yes</option>
+        //                 <option value="false">No</option>
+        //             </select><br/>
+        //             <select onChange={this.onFormChange} className="car-type" name="typeOfCar">
+        //                 <option value="">Type of Car</option>
+        //                 <option value="midsize_suv">Midsize SUV</option>
+        //                 <option value="large_suv">Large SUV</option>
+        //                 <option value="truck">Truck</option>
+        //                 <option value="compact_car">Compact Car</option>
+        //                 <option value="midsize_car">Midsize Car</option>
+        //                 <option value="electric">Electric Car</option>
+        //             </select><br/>
+        //             <input onChange={this.onFormChange} className="car-mpg" name="mpg" type="text" placeholder="Enter MPG..."/><br/>
+        //             <select onChange={this.onFormChange} className="car-charge" name="chargeAtHome">
+        //                 <option value="">Charge at home?</option>
+        //                 <option value="true">Yes</option>
+        //                 <option value="false">No</option>
+        //             </select><br/>
+        //             <input className="car-submit" name="submit" type="submit" value="See Recommendations"/>
+        //         </form>
+        //     </>
+        // )
         return(
-            <>
-                <h2 className="car-form-title">Car Information</h2>
-                <form onSubmit={this.formSubmit} className="car-form">
-                    <input onChange={this.onFormChange} className="car-name" name="name" type="text" placeholder="Enter Name..."/><br/>
-                    <input onChange={this.onFormChange} className="car-email" name="email" type="text" placeholder="Enter Email..."/><br/>
-                    <select onChange={this.onFormChange} className="car-ownership" name="ownACar">
-                        <option value="">Do you own a car?</option>
+            <div className="new-car-form">
+                <h3>Car Sustainability Helper</h3>
+            <Form>
+                <Form.Group controlId="car-name">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control onChange={this.onFormChange} name="name" type="text" />
+                </Form.Group>
+                <Form.Group controlId="car-email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control onChange={this.onFormChange} name="email" type="text" />
+                </Form.Group>
+                <Form.Group controlId="car-ownership">
+                    <Form.Label>Do you own a car?</Form.Label>
+                    <Form.Control as="select" onChange={this.onFormChange} name="ownACar">
+                        <option value="">Please Select...</option>
                         <option value="true">Yes</option>
                         <option value="false">No</option>
-                    </select><br/>
-                    <select onChange={this.onFormChange} className="car-type" name="typeOfCar">
-                        <option value="">Type of Car</option>
+                    </Form.Control>
+                </Form.Group>
+                <Form.Group controlId="car-type">
+                    <Form.Label>Type of Car?</Form.Label>
+                    <Form.Control as="select" onChange={this.onFormChange} name="typeOfCar">
+                        <option value="">Please Select...</option>
                         <option value="midsize_suv">Midsize SUV</option>
                         <option value="large_suv">Large SUV</option>
                         <option value="truck">Truck</option>
                         <option value="compact_car">Compact Car</option>
                         <option value="midsize_car">Midsize Car</option>
                         <option value="electric">Electric Car</option>
-                    </select><br/>
-                    <input onChange={this.onFormChange} className="car-mpg" name="mpg" type="text" placeholder="Enter MPG..."/><br/>
-                    <select onChange={this.onFormChange} className="car-charge" name="chargeAtHome">
-                        <option value="">Charge at home?</option>
+                    </Form.Control>
+                </Form.Group>
+                <Form.Group controlId="car-mpg">
+                    <Form.Label>Car Mpg</Form.Label>
+                    <Form.Control onChange={this.onFormChange} name="mpg" type="text" />
+                </Form.Group>
+                <Form.Group controlId="car-charge">
+                <Form.Label>Ability to charge at home?</Form.Label>
+                    <Form.Control as="select" onChange={this.onFormChange} name="chargeAtHome">
+                        <option value="">Please Select...</option>
                         <option value="true">Yes</option>
                         <option value="false">No</option>
-                    </select><br/>
-                    <input className="car-submit" name="submit" type="submit" value="See Recommendations"/>
-                </form>
-            </>
+                    </Form.Control>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    See Recommendations
+                </Button>
+            </Form>
+            </div>
         )
-        // return(
-        //     <Form>
-        //         <FormGroup>
-        //             <ControlLabel>Name</ControlLabel>
-        //             <FormControl as="input" onChange={(event) => this.onFormChange(event)} className="car-name" name="name" />
-        //             <HelpBlock>Required</HelpBlock>
-        //         </FormGroup>
-        //         <FormGroup>
-        //             <ControlLabel>Email</ControlLabel>
-        //             <FormControl onChange={this.onFormChange} className="car-email" name="email" type="text" />
-        //             <HelpBlock tooltip>Required</HelpBlock>
-        //         </FormGroup>
-        //         <FormGroup>
-        //             <Dropdown onChange={this.onFormChange} title="Do you own a car?" className="car-ownership" name="ownACar">
-        //                 <Dropdown.Item value="true">Yes</Dropdown.Item>
-        //                 <Dropdown.Item value="false">No</Dropdown.Item>
-        //             </Dropdown>
-        //         </FormGroup>
-        //         <FormGroup>
-        //             <Dropdown onChange={this.onFormChange} title="Car Type" className="car-type" name="typeOfCar">
-        //                 <Dropdown.Item value="midsize_suv">Midsize SUV</Dropdown.Item>
-        //                 <Dropdown.Item value="large_suv">Large SUV</Dropdown.Item>
-        //                 <Dropdown.Item value="truck">Truck</Dropdown.Item>
-        //                 <Dropdown.Item value="compact_car">Compact Car</Dropdown.Item>
-        //                 <Dropdown.Item value="midsize_car">Midsize Car</Dropdown.Item>
-        //                 <Dropdown.Item value="electric">Electric Car</Dropdown.Item>
-        //             </Dropdown>
-        //         </FormGroup>
-        //         <FormGroup>
-        //             <ControlLabel>Car Mpg</ControlLabel>
-        //             <FormControl onChange={this.onFormChange} className="car-mpg" name="mpg" type="text" />
-        //         </FormGroup>
-        //         <FormGroup>
-        //             <Dropdown onChange={this.onFormChange} title="Ability to charge car at home?" className="car-charge" name="chargeAtHome">
-        //                 <Dropdown.Item value="true">Yes</Dropdown.Item>
-        //                 <Dropdown.Item value="false">No</Dropdown.Item>
-        //             </Dropdown>
-        //         </FormGroup>
-        //         <FormGroup>
-        //         <ButtonToolbar>
-        //             <Button appearance="primary">Submit</Button>
-        //             <Button appearance="default">Cancel</Button>
-        //         </ButtonToolbar>
-        //         </FormGroup>
-        //     </Form>
-        // )
     }
 }
 export default CarForm
